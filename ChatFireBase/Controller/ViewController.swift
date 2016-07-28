5f11889fbed9c8fd9908d5daee7dfc0b00b018a0
Bao Nguyen <baonguyena1@gmail.com>
2016-07-28 20:35:45 +0700

    - Get all user

--- ChatFireBase/Controller/ViewController.swift
+++ ChatFireBase/Controller/ViewController.swift
@@ -15,13 +15,32 @@ class MessagesController: UITableViewController {
         super.viewDidLoad()
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: #selector(handleLogout))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "new_message_icon"), style: .Plain, target: self, action: #selector(handleNewMessage))
         
         checkIfUserIsLoggedIn()
+    }
+    
+    func handleNewMessage() {
+        let newMessageController = NewMessageController()
+        let navigationController = UINavigationController(rootViewController: newMessageController)
+        presentViewController(navigationController, animated: true, completion: nil)
     }
     
     func checkIfUserIsLoggedIn() {
         if (FIRAuth.auth()?.currentUser?.uid == nil) {
             performSelector(#selector(handleLogout), withObject: nil, afterDelay: 0)
+        } else {
+            Common.makeIndicator(self.view)
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            FIRDatabase.database().reference().child("users").child(uid!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+                
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = dictionary["name"] as? String
+                }
+                Common.hideIndicator()
+                }, withCancelBlock: { (error) in
+                    Common.hideIndicator()
+            })
         }
     }
 
---
+++ ChatFireBase/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ChatFireBase
+//
+//  Created by Bao Nguyen on 7/28/16.
+//  Copyright © 2016 baon. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+}
